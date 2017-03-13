62c4bff376605e54875cad06d97285e259664467
The-BB <tun.chen.bo@gmail.com>
2017-03-13 11:44:27 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EK4tests
+
+Этот репозиторий для маршрутизаторов ZyXEL Keenetic.
+
+---
 для тестов
+---
+Entware-Keenetic repository only for tests new libs, tools, packages, etc.
+---
+
+[Использование](https://github.com/The-BB/Entware-Keenetic/wiki).
---
+++ include/kernel-version.mk
@@ -0,0 +1,19 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+LINUX_RELEASE?=1
+
+LINUX_VERSION-2.6.22 = -tc
+
+ifdef KERNEL_PATCHVER
+  LINUX_VERSION:=$(KERNEL_PATCHVER)$(strip $(LINUX_VERSION-$(KERNEL_PATCHVER)))
+endif
+
+split_version=$(subst ., ,$(1))
+merge_version=$(subst $(space),.,$(1))
+KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+KERNEL_PATCHVER ?= $(KERNEL)
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_HASH:=$(LINUX_KERNEL_HASH-$(strip $(LINUX_VERSION)))
+LINUX_KERNEL_HASH?=x
